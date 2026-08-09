91376439c16c6496b71617e3d9bb77a8492aaee6
ablegamez <54194770+ablegamez@users.noreply.github.com>
2026-08-09 17:07:31 -0500

    Update basic.js

--- basic.js
+++ basic.js
@@ -8,256 +8,179 @@ Yac Nursing
 
 */
 
- 
+window.onload = function () {
+  //alert("Onload");
 
-window.onload = function(){
+  startTime = Math.floor(Math.random() * (1045 - 1007 + 1)) + 1007;
 
-//alert("Onload");
+  document.getElementById("initialText").value = startTime.toString();
+};
 
-startTime = Math.floor(Math.random() * (1045 - 1007 + 1)) + 1007;
-
-document.getElementById("initialText").value = startTime.toString();
-
-}
-
- 
-
-function hello(){
-
+function hello() {
   alert("Hello World!!!");
-
 }
 
- 
+function getRandomTimes() {
+  console.log("Visit and Travel times:\n");
 
-function getRandomTimes(){
+  winValue = document.getElementById("initialText").value;
 
-               console.log("Visit and Travel times:\n");
+  document.getElementById("endVisit1txt").value = returnVisitTime(winValue);
 
-winValue = document.getElementById("initialText").value;
+  nextTimeStart = document.getElementById("endVisit1txt").value;
 
-document.getElementById("endVisit1txt").value = returnVisitTime(winValue);
+  document.getElementById("startVisit2txt").value =
+    startVisitTime(nextTimeStart);
 
- 
+  nextTimeStart = document.getElementById("startVisit2txt").value;
 
-nextTimeStart = document.getElementById("endVisit1txt").value;
+  document.getElementById("endVisit2txt").value =
+    returnVisitTime(nextTimeStart);
 
-document.getElementById("startVisit2txt").value = startVisitTime(nextTimeStart);
+  nextTimeStart = document.getElementById("endVisit2txt").value;
 
-nextTimeStart = document.getElementById("startVisit2txt").value;
+  document.getElementById("startVisit3txt").value =
+    startVisitTime(nextTimeStart);
 
-document.getElementById("endVisit2txt").value = returnVisitTime(nextTimeStart);
+  nextTimeStart = document.getElementById("startVisit3txt").value;
 
- 
-
-nextTimeStart = document.getElementById("endVisit2txt").value;
-
-document.getElementById("startVisit3txt").value = startVisitTime(nextTimeStart);
-
-nextTimeStart = document.getElementById("startVisit3txt").value;
-
-document.getElementById("endVisit3txt").value = returnVisitTime(nextTimeStart);
-
+  document.getElementById("endVisit3txt").value =
+    returnVisitTime(nextTimeStart);
 }
 
- 
+function returnVisitTime(initialTime) {
+  if (initialTime.length == 3) {
+    initialTime = "0" + initialTime;
+  }
 
-function returnVisitTime (initialTime) {
+  hours = parseInt(initialTime.slice(0, 2));
 
-if (initialTime.length == 3){
+  mins = parseInt(initialTime.slice(2));
 
-               initialTime = "0" + initialTime;
+  endVisit = Math.floor(Math.random() * (80 - 75 + 1)) + 75;
 
+  console.log("Visit: " + endVisit);
+
+  mins = mins + endVisit;
+
+  if (mins + endVisit == 60) {
+    endHours++;
+
+    endVisit = 0;
+  } else if (mins == 120) {
+    hours++;
+
+    hours++;
+
+    mins = 0;
+  } else if (mins > 120) {
+    hours++;
+
+    hours++;
+
+    mins = mins - 120;
+  } else if (mins > 60) {
+    hours++;
+
+    mins = mins - 60;
+  }
+
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  endTime = hours.toString() + "" + mins.toString();
+
+  return endTime;
 }
 
- 
+function startVisitTime(initialTime) {
+  //console.log(initialTime.slice(0, 2));
 
-hours = parseInt(initialTime.slice(0,2));
+  if (initialTime.length == 3) {
+    initialTime = "0" + initialTime;
+  }
 
-mins = parseInt(initialTime.slice(2));
+  hours = parseInt(initialTime.slice(0, 2));
 
- 
+  mins = parseInt(initialTime.slice(2));
 
-endVisit=Math.floor(Math.random() * (80 - 75 + 1)) + 75;
+  endVisit = Math.floor(Math.random() * (50 - 35 + 1)) + 35;
 
-console.log("Visit: " + endVisit);
+  console.log("Travel: " + endVisit);
 
- 
+  mins = mins + endVisit;
 
-mins = mins + endVisit;
+  if (mins + endVisit == 60) {
+    endHours++;
 
- 
+    endVisit = 0;
+  } else if (mins == 120) {
+    hours++;
 
-               if (mins + endVisit == 60) {
+    hours++;
 
-                              endHours++;
+    mins = 0;
+  } else if (mins > 120) {
+    hours++;
 
-                              endVisit = 0;
+    hours++;
 
-               }
+    mins = mins - 120;
+  } else if (mins > 60) {
+    hours++;
 
-               else if (mins == 120) {
+    mins = mins - 60;
+  }
 
-                              hours++;
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
 
-                              hours++;
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
 
-                              mins = 0;
+  endTime = hours.toString() + "" + mins.toString();
 
-               }
-
-               else if (mins > 120) {
-
-                              hours++;
-
-                              hours++;
-
-                              mins = (mins) - 120;
-
-               }
-
-               else if (mins > 60) {
-
-                              hours++;
-
-                              mins = (mins) - 60;
-
-               }
-
-if (mins < 10){
-
-               mins = "0" + mins;
-
+  return endTime;
 }
 
- 
+function getBmi() {
+  console.log("BMI Calculator:");
 
-if (hours < 10){
+  bmiWeight = document.getElementById("bmiWt").value;
 
-               hours = "0" + hours;
+  bmiHeight = document.getElementById("bmiHt").value;
 
+  bmiLevel = (bmiWeight / (bmiHeight * bmiHeight)) * 703;
+
+  bmiLevel = (Math.round(bmiLevel * 100) / 100).toFixed(2);
+
+  console.log(
+    "Wt: " +
+      bmiWeight +
+      "lbs + " +
+      "Ht: " +
+      bmiHeight +
+      "inches = " +
+      "BMI of " +
+      bmiLevel,
+  );
+
+  document.getElementById("bmiTxt").value = bmiLevel;
 }
 
- 
+const genderCheckbox = document.getElementById("genderCheck");
 
-endTime = hours.toString() + "" + mins.toString();
-
-return endTime;
-
-}
-
- 
-
-function startVisitTime (initialTime) {
-
-//console.log(initialTime.slice(0, 2));
-
- 
-
-if (initialTime.length == 3){
-
-               initialTime = "0" + initialTime;
-
-}
-
- 
-
-hours = parseInt(initialTime.slice(0,2));
-
-mins = parseInt(initialTime.slice(2));
-
- 
-
-endVisit=Math.floor(Math.random() * (50 - 35 + 1)) + 35;
-
-console.log("Travel: " + endVisit);
-
- 
-
-mins = mins + endVisit;
-
- 
-
-               if (mins + endVisit == 60) {
-
-                              endHours++;
-
-                              endVisit = 0;
-
-               }
-
-               else if (mins == 120) {
-
-                              hours++;
-
-                              hours++;
-
-                              mins = 0;
-
-               }
-
-               else if (mins > 120) {
-
-                              hours++;
-
-                              hours++;
-
-                              mins = (mins) - 120;
-
-               }
-
-               else if (mins > 60) {
-
-                              hours++;
-
-                              mins = (mins) - 60;
-
-               }
-
- 
-
-if (mins < 10){
-
-               mins = "0" + mins;
-
-}
-
- 
-
-if (hours < 10){
-
-               hours = "0" + hours;
-
-}
-
- 
-
-endTime = hours.toString() + "" + mins.toString();
-
-return endTime;
-
- 
-
-}
-
- 
-
-function getBmi(){
-
-               console.log("BMI Calculator:")
-
-               bmiWeight = document.getElementById("bmiWt").value;
-
-               bmiHeight = document.getElementById("bmiHt").value;
-
-               bmiLevel = (bmiWeight/(bmiHeight * bmiHeight)) * 703;
-
-               bmiLevel = (Math.round(bmiLevel * 100)/100).toFixed(2);
-
-               console.log ("Wt: " + bmiWeight + "lbs + " + "Ht: " + bmiHeight + "inches = " + "BMI of " + bmiLevel);
-
-               document.getElementById("bmiTxt").value = bmiLevel;
-
-}
-
- 
+genderCheckbox.addEventListener("change", function () {
+  if (this.checked) {
+    alert("Checkbox is checked");
+  } else {
+    alert("Checkbox is unchecked");
+  }
+});
